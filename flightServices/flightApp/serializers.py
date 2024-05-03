f6dae3308108d32eecd17582156a4c653d5931eb
akhil <akhilboyanapalli657@gmail.com>
2024-05-03 15:20:01 -0500

    custom validators for input data

--- flightServices/flightApp/serializers.py
+++ flightServices/flightApp/serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
 from flightApp.models import Flight,Passenger,Reservation
+import re
+
+def isFlightNumberValid(data):
+  print(data)
+  print("isFlightNumberValid")
 
 class FlightSerializer(serializers.ModelSerializer):
   class Meta:
     model = Flight
     fields = '__all__'
+    validators = [isFlightNumberValid]
+  
+  def validate_flightNumber(self,flightNumber):
+    print("validate_flightNumber")
+    if(re.match("^[a-zA-Z0-9]*$",flightNumber)==None):
+      raise serializers.ValidationError("Invalid flight number. It should only be alpha numeric")
+    return flightNumber
+  
+  def validate(self,data):
+    print("validate")
+    print(data['flightNumber'])
+    return data
+
+
 
 class PassengerSerializer(serializers.ModelSerializer):
   class Meta:
